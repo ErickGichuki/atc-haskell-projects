59329ff3012ffc9a33195b7ef8aab1fd9c949679
+ <+>
2025-01-08 19:33:49 +0300

    feat:add the 'add' logic

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,17 @@
 module Main where
 
 import System.IO (hFlush, stdout)
+import System.Directory (doesFileExist, removeFile)
 
 main :: IO ()
 main = do
   putStrLn "Welcome to my TODO List Manager!"
+  ensureTasksFile
   loop
 
 loop :: IO ()
 loop = do
-  putStr "Enter command: "
+  putStr "Enter command (add/view/edit/delete/complete/exit/help): "
   hFlush stdout
   input <- getLine
   isLooping <- handleInput input
@@ -21,6 +23,12 @@ handleInput :: String -> IO Bool
 handleInput "exit" = do
   putStrLn "Goodbye!"
   pure False
-handleInput input = do
-  putStrLn $ "You entered: " ++ input
+handleInput "add" = do
+  putStr "Enter task description: "
+  hFlush stdout
+  desc <- getLine
+  addTask desc
+  pure True
+handleInput "view" = do
+  viewTasks
   pure True

1d63ac698b6ca420d2c00e3b1afdc823e62b4a9e
+ <+>
2025-01-09 11:20:18 +0300

    fix:fix an issue with the modules

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,12 @@
 module Main where
 
 import System.IO (hFlush, stdout)
-import FileHandler (ensureTasksFile)
-import UserInteraction (displayHelp, handleInput)
+import qualified TaskManager as TM
 
 main :: IO ()
 main = do
   putStrLn "Welcome to my TODO List Manager!"
-  ensureTasksFile "tasks.txt"
-  displayHelp
+  TM.ensureTasksFile
   loop
 
 loop :: IO ()
@@ -20,3 +18,47 @@ loop = do
   if isLooping
     then loop
     else return ()
+
+handleInput :: String -> IO Bool
+handleInput "exit" = do
+  putStrLn "Goodbye!"
+  pure False
+handleInput "add" = do
+  putStr "Enter task description: "
+  hFlush stdout
+  desc <- getLine
+  TM.addTask desc
+  pure True
+handleInput "view" = do
+  TM.viewTasks
+  pure True
+handleInput "edit" = do
+  putStr "Enter task number to edit: "
+  hFlush stdout
+  num <- getLine
+  putStr "Enter new description: "
+  hFlush stdout
+  desc <- getLine
+  TM.editTask (read num) desc
+  pure True
+handleInput "complete" = do
+  putStr "Enter task number to mark as complete: "
+  hFlush stdout
+  num <- getLine
+  TM.completeTask (read num)
+  pure True
+handleInput "help" = do
+  putStrLn "TODO List Manager Commands:"
+  putStrLn "  add      - Add a new task"
+  putStrLn "  view     - View all tasks"
+  putStrLn "  edit     - Edit a task"
+  putStrLn "  delete   - Delete a task"
+  putStrLn "  complete - Mark a task as complete"
+  putStrLn "  exit     - Exit the program"
+  putStrLn "  help     - Show this help menu"
+  pure True
+handleInput "-h" = handleInput "help"
+handleInput "--help" = handleInput "help"
+handleInput input = do
+  putStrLn $ "Unknown command: " ++ input
+  pure True
--- src/TaskManager.hs
+++ src/TaskManager.hs
@@ -1,18 +1,25 @@
-module TaskManager (addTask, viewTasks, editTask, deleteTask, completeTask) where
+module TaskManager where
 
-import FileHandler (readTasks, writeTasks)
+import System.Directory (doesFileExist)
+import System.IO (writeFile, appendFile, readFile)
+
+-- Ensure the tasks file exists
+ensureTasksFile :: IO ()
+ensureTasksFile = do
+  exists <- doesFileExist "tasks.txt"
+  if not exists
+    then writeFile "tasks.txt" ""
+    else return ()
 
 -- Add a new task
 addTask :: String -> IO ()
-addTask desc = do
-  tasks <- readTasks
-  writeTasks (tasks ++ [desc ++ " | Incomplete"])
-  putStrLn "Task added!"
+addTask desc = appendFile "tasks.txt" (desc ++ " | Incomplete\n") >> putStrLn "Task added!"
 
 -- View all tasks
 viewTasks :: IO ()
 viewTasks = do
-  tasks <- readTasks
+  contents <- readFile "tasks.txt"
+  let tasks = lines contents
   if null tasks
     then putStrLn "No tasks found."
     else mapM_ putStrLn $ zipWith (\i t -> show i ++ ". " ++ t) [1 :: Int ..] tasks
@@ -20,34 +27,37 @@ viewTasks = do
 -- Edit a task
 editTask :: Int -> String -> IO ()
 editTask num newDesc = do
-  tasks <- readTasks
+  contents <- readFile "tasks.txt"
+  let tasks = lines contents
   if num <= 0 || num > length tasks
     then putStrLn "Invalid task number."
     else do
-      let updatedTasks = map (\(i, t) -> if i == num then newDesc ++ " | Incomplete" else t) $ zip [1 :: Int ..] tasks
-      writeTasks updatedTasks
+      let updatedTasks = unlines $ map (\(i, t) -> if i == num then newDesc ++ " | Incomplete" else t) $ zip [1 :: Int ..] tasks
+      writeFile "tasks.txt" updatedTasks
       putStrLn "Task updated!"
 
 -- Delete a task
 deleteTask :: Int -> IO ()
 deleteTask num = do
-  tasks <- readTasks
+  contents <- readFile "tasks.txt"
+  let tasks = lines contents
   if num <= 0 || num > length tasks
     then putStrLn "Invalid task number."
     else do
-      let updatedTasks = map snd $ filter (\(i, _) -> i /= num) $ zip [1 :: Int ..] tasks
-      writeTasks updatedTasks
+      let updatedTasks = unlines $ map snd $ filter (\(i, _) -> i /= num) $ zip [1 :: Int ..] tasks
+      writeFile "tasks.txt" updatedTasks
       putStrLn "Task deleted!"
 
 -- Mark a task as completed
 completeTask :: Int -> IO ()
 completeTask num = do
-  tasks <- readTasks
+  contents <- readFile "tasks.txt"
+  let tasks = lines contents
   if num <= 0 || num > length tasks
     then putStrLn "Invalid task number."
     else do
-      let updatedTasks = map (\(i, t) -> if i == num then markComplete t else t) $ zip [1 :: Int ..] tasks
-      writeTasks updatedTasks
+      let updatedTasks = unlines $ map (\(i, t) -> if i == num then markComplete t else t) $ zip [1 :: Int ..] tasks
+      writeFile "tasks.txt" updatedTasks
       putStrLn "Task marked as complete!"
 
 -- Helper to mark a task as completed
